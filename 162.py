3d7512bc01c5e6f3097d7a078d3fb182bfbeb7a0
Ebele-Abolo <77888430+Ebele-Abolo@users.noreply.github.com>
2023-07-22 17:23:40 -0500

    Add files via upload

---
+++ 162.py
@@ -0,0 +1,7 @@
+import webbrowser
+file = open(input_name+".html", "w")
+data = my_text.get("1.0",END)
+input_file_name.delete(0, END)
+my_text.delete(1.0,END)
+webbrowser.open(name)
+webbrowser.open_new('file://' + file_path)
